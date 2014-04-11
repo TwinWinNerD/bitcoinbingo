090a217d8796c3b84fa10d276a76fbcd3f5101c7
Martijn Buurman <martijn@m19.nl>
2014-04-11 17:45:19 +0200

    Added Query Service

---
+++ api/services/QueryService.js
@@ -0,0 +1,10 @@
+module.exports = function (whereStatement, includeStatement, eagerLoading) {
+
+    if (whereStatement) {
+        this.where = whereStatement;
+    }
+
+    if (eagerLoading === true) {
+        this.include = includeStatement;
+    }
+};
