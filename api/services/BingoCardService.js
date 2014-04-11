2ff436fc6e1a7298586011b01f55aefeca4aad89
Martijn Buurman <martijn@m19.nl>
2014-04-11 16:36:15 +0200

    Added feature to create bingoCard squares

---
+++ api/services/BingoCardService.js
@@ -0,0 +1,50 @@
+var crypto;
+
+crypto = require('crypto');
+
+exports.generateSquares = function (serverSeed, clientSeed, nonce) {
+
+    var squares, hash, squareIndex, characterIndex, subsitute, decimal, hashCharacters;
+
+    squares = [ [], [], [], [], [] ];
+
+    hash = exports.generateCardHash(serverSeed, clientSeed, nonce);
+    hashCharacters = hash.split('');
+
+    squareIndex = 0;
+    characterIndex = 0;
+    subsitute = 0;
+
+    while (squareIndex < squares.length) {
+
+        while (characterIndex < hashCharacters.length) {
+
+            decimal = parseInt(hashCharacters[characterIndex], 16);
+
+            if (squares[squareIndex]) {
+
+                if (decimal !== 0 && squares[squareIndex].length < 5 && squares[squareIndex].indexOf(decimal + subsitute) === -1) {
+
+                    if (squareIndex === 2 && squares[2].length === 2) {
+                        squares[squareIndex].push(0);
+                    } else {
+                        decimal += subsitute;
+                        squares[squareIndex].push(decimal);
+                    }
+
+                } else if (squares[squareIndex].length === 5) {
+                    subsitute += 15;
+                    squareIndex++;
+                }
+            }
+
+            characterIndex++;
+        }
+    }
+
+    return squares;
+};
+
+exports.generateCardHash = function (serverSeed, clientSeed, nonce) {
+    return crypto.createHash('sha512').update(serverSeed + ":" + clientSeed + ":" + nonce).digest('hex');
+};
