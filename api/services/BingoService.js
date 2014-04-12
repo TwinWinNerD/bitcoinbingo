f896d57ed1be2409f6fc7f5a12d904a4a676e9d5
Martijn Buurman <martijn@m19.nl>
2014-04-12 13:58:15 +0200

    Created and small changes
    Created calculatePrizePool function
    Created awardWinners function
    Small changes

--- api/services/BingoService.js
+++ api/services/BingoService.js
@@ -1,7 +1,8 @@
-var Q, _;
+var Q, _, async;
 
 Q = require('q');
 _ = require('lodash');
+async = require('async');
 
 exports.settleRound = function (gameId, instant) {
 
@@ -26,7 +27,20 @@ exports.settleRound = function (gameId, instant) {
             }
 
             exports.runSimulation(game, instant).then( function (winners) {
-                deferred.resolve(winners);
+
+                if(awardPrizes) {
+                    console.log("award prizes");
+                    rewardWinners(game, winners).then( function (result) {
+                        if(result) {
+
+                            deferred.resolve(winners);
+
+                        }
+                    });
+                } else {
+                    deferred.resolve(winners);
+                }
+
             });
         }
     });
@@ -46,7 +60,7 @@ exports.runSimulation = function (game, instant) {
         bingoCard.squares = BingoCardService.generateSquares(game.serverseed, bingoCard.clientSeed, bingoCard.id);
     }
 
-    var bingoNumbers, masterSeed, bingoWinners, patternWinners, turn, drawnNumbers;
+    var bingoNumbers, masterSeed, bingoWinners, patternWinners, winners, turn, drawnNumbers;
 
     bingoNumbers = new BingoNumbersService();
     masterSeed = SeedService.generateMasterSeed(game);
@@ -54,6 +68,7 @@ exports.runSimulation = function (game, instant) {
 
     bingoWinners = [];
     patternWinners = [];
+    winners = [];
 
     drawnNumbers = [];
     turn = 0;
@@ -65,16 +80,16 @@ exports.runSimulation = function (game, instant) {
 
                 drawnNumbers.push(number);
 
-                bingoWinners = checkForWinners('bingo', game.bingoCards, drawnNumbers);
-                patternWinners = checkForWinners('pattern', game.bingoCards, drawnNumbers);
+                // using cloneDeep to prevent the pattern check overwriting the bingo check
+                bingoWinners = _.cloneDeep(checkForWinners('bingo', game.bingoCards, drawnNumbers));
+                patternWinners = _.cloneDeep(checkForWinners('pattern', game.bingoCards, drawnNumbers));
 
                 console.log(number);
 
-                console.log(patternWinners);
-                console.log(bingoWinners);
-
                 if (bingoWinners.length > 0) {
-                    deferred.resolve({ bingoWinners: bingoWinners, patternWinners: patternWinners });
+                    winners.push(bingoWinners);
+                    winners.push(patternWinners);
+                    deferred.resolve(winners);
                 } else {
                     turn++;
                     playTurn();
@@ -93,14 +108,13 @@ exports.runSimulation = function (game, instant) {
             number = instantlyDrawNumber(bingoNumbers, turn);
             drawnNumbers.push(number);
 
-            bingoWinners = checkForWinners('bingo', game.bingoCards, drawnNumbers);
-            patternWinners = checkForWinners('pattern', game.bingoCards, drawnNumbers);
-
-            console.log(bingoWinners);
-            console.log(patternWinners);
+            bingoWinners = _.cloneDeep(checkForWinners('bingo', game.bingoCards, drawnNumbers));
+            patternWinners = _.cloneDeep(checkForWinners('pattern', game.bingoCards, drawnNumbers));
 
             if (bingoWinners.length > 0) {
-                deferred.resolve({ bingoWinners: bingoWinners, patternWinners: patternWinners });
+                winners.push(bingoWinners);
+                winners.push(patternWinners);
+                deferred.resolve(winners);
                 break;
             } else {
                 turn++;
@@ -173,16 +187,94 @@ function checkForWinners (type, cards, drawnNumbers) {
 
         if(patternCount === numbersForPattern) {
             cards[x].won = "pattern";
+
             winners.push(cards[x]);
         }
     }
 
     if(winners.length > 0) {
 
-        winners = _.uniq(winners, function (winner) { return JSON.stringify(winner); });
-
         return winners;
     }
 
     return [];
+}
+
+function rewardWinners (game, winners) {
+    var deferred, prizePool, patternPercentage, bingoPercentage, winnerData, depositData;
+    deferred = Q.defer();
+
+    prizePool = calculatePrizePool(game);
+    patternPercentage = 0.30;
+    bingoPercentage = 0.70;
+
+    winnerData = [];
+    depositData = [];
+
+    for(var i = 0; i < winners.length; i++) {
+        var amountOfWinners;
+
+        amountOfWinners = winners[i].length;
+
+        for(var j = 0; j < amountOfWinners; j++) {
+            var winner, winningCard, amount;
+            winningCard = winners[i][j];
+            winner = {};
+
+            winner.winnerType = winningCard.won;
+            winner.player = winningCard.player;
+            winner.game = winningCard.game;
+
+            if(winner.winnerType === "bingo") {
+                amount = parseInt((prizePool * bingoPercentage) / amountOfWinners, 10);
+            } else if(winner.winnerType === "pattern") {
+                amount = parseInt((prizePool * patternPercentage) / amountOfWinners, 10);
+            }
+
+            winner.amount = amount;
+
+            winnerData.push(winner);
+
+            depositData.push({
+                amount: winner.amount,
+                    type: "winnings",
+                player: winner.player
+            });
+        }
+    }
+
+    async.parallel([
+        function (cb) {
+            Winner.create(winnerData).exec( function (error, result) {
+                if(!error) {
+                    cb();
+                }
+            });
+        },
+        function (cb) {
+            Deposit.create(depositData).exec( function (error, result) {
+                if(!error) {
+                    cb();
+                }
+            });
+        }
+    ], function (error, results) {
+        if(!error) {
+            deferred.resolve(results);
+        } else {
+            deferred.reject(error);
+        }
+    });
+
+    return deferred.promise;
+}
+
+function calculatePrizePool(game) {
+
+    var cardPrice, amountOfCards;
+
+    cardPrice = game.table.cardPrice;
+    amountOfCards = game.bingoCards.length;
+
+    return (cardPrice * amountOfCards) * 0.95;
 }
