a1007442eb7efb0293811de239bc2f544d336245
Martijn Buurman <martijn@m19.nl>
2014-04-14 21:14:31 +0200

    Added buyCard action

--- assets/js/scripts/controllers/GameController.js
+++ assets/js/scripts/controllers/GameController.js
@@ -1,5 +1,9 @@
 App.GameController = Ember.ObjectController.extend({
 
+    setupController: function (controller, model) {
+        controller.set('model', model);
+    },
+
     idle: function () {
         if(this.get('model.gameStatus') === 'idle') {
             return true;
@@ -19,5 +23,27 @@ App.GameController = Ember.ObjectController.extend({
             return true;
         }
         return false;
-    }.property('model.gameStatus')
+    }.property('model.gameStatus'),
+
+    actions: {
+
+        buyCard: function () {
+            var store, game, bingoCard;
+
+
+
+            store = this.store;
+            game = this.get('model');
+
+
+            bingoCard = store.createRecord('bingoCard', {
+                clientSeed: "12345",
+                game: game
+            });
+
+            bingoCard.save();
+
+        }
+
+    }
 });
