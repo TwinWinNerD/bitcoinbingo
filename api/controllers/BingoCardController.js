1a679816daf6043bf1dc6dc86295e44729933f99
Martijn Buurman <martijn@m19.nl>
2014-04-16 19:43:57 +0200

    Added create method for bingo cards

--- api/controllers/BingoCardController.js
+++ api/controllers/BingoCardController.js
@@ -40,8 +40,46 @@ module.exports = {
 
             res.ok(result);
         });
+    },
 
+    create: function(req, res) {
+        var Model = actionUtil.parseModel(req);
+
+        // Create data object (monolithic combination of all parameters)
+        // Omit the blacklisted params (like JSONP callback param, etc.)
+        var data = actionUtil.parseValues(req);
+
+        if(typeof req.user.id !== "undefined") {
+            data.user = req.user.id;
+
+            // TODO: determine price, create deposit etc
+
+            // Create new instance of model using data from params
+            Model.create(data).exec(function created (err, newInstance) {
+
+                // Differentiate between waterline-originated validation errors
+                // and serious underlying issues. Respond with badRequest if a
+                // validation error is encountered, w/ validation info.
+                if (err) return res.negotiate(err);
+
+                // If we have the pubsub hook, use the model class's publish method
+                // to notify all subscribers about the created item
+                if (req._sails.hooks.pubsub) {
+                    if (req.isSocket) {
+                        Model.subscribe(req, newInstance);
+                        Model.introduce(newInstance);
+                    }
+
+                    Model.publishCreate(newInstance, !req.options.mirror && req);
+                }
+
+                // Send JSONP-friendly response if it's supported
+                // (HTTP 201: Created)
+                res.status(201);
+                res.ok(newInstance.toJSON());
+            });
+        } else {
+            res.forbidden('Need to be logged in to buy cards');
+        }
     }
-
-
 };
