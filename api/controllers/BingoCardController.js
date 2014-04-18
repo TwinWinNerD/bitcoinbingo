aa8ced61b92cd3376ac811392db28ea5d2990e53
Martijn Buurman <martijn@m19.nl>
2014-04-18 10:47:03 +0200

    Changed bingoCard create action
    Now checking:
    - the prize of the cards
    - valid user balance
    - creating a withdrawal
    - creating the actual card

--- api/controllers/BingoCardController.js
+++ api/controllers/BingoCardController.js
@@ -5,9 +5,10 @@
  * @docs        :: http://sailsjs.org/#!documentation/controllers
  */
 
-var actionUtil;
+var actionUtil, async;
 
 actionUtil = require('../actionUtil');
+async = require('async');
 
 module.exports = {
 
@@ -49,37 +50,95 @@ module.exports = {
         // Omit the blacklisted params (like JSONP callback param, etc.)
         var data = actionUtil.parseValues(req);
 
-        if(typeof req.user.id !== "undefined") {
+        if(typeof req.user !== "undefined") {
             data.user = req.user.id;
 
-            // TODO: determine price, create deposit etc
-
-            // Create new instance of model using data from params
-            Model.create(data).exec(function created (err, newInstance) {
-
-                // Differentiate between waterline-originated validation errors
-                // and serious underlying issues. Respond with badRequest if a
-                // validation error is encountered, w/ validation info.
-                if (err) return res.negotiate(err);
-
-                // If we have the pubsub hook, use the model class's publish method
-                // to notify all subscribers about the created item
-                if (req._sails.hooks.pubsub) {
-                    if (req.isSocket) {
-                        Model.subscribe(req, newInstance);
-                        Model.introduce(newInstance);
-                    }
-
-                    Model.publishCreate(newInstance, !req.options.mirror && req);
+            async.parallel({
+                totalPrize: function(done) {
+                    // use amountOfCards = 1 for now
+                    BingoCardService.calculateTotalPrice(data.game, 1).then( function (totalPrize) {
+                        if(totalPrize !== null) {
+                            done(null, totalPrize);
+                        }
+                    }, function (error) {
+                        if(error) {
+                            done(error);
+                        }
+                    });
+                },
+                userBalance: function(done) {
+                    UserService.getBalance(data.user).then(function (balance) {
+                        if(balance !== null) {
+                            done(null, balance);
+                        }
+                    }, function(error) {
+                        if(error) {
+                            done(error);
+                        }
+                    });
                 }
+            }, function (error, results) {
 
-                // Send JSONP-friendly response if it's supported
-                // (HTTP 201: Created)
-                res.status(201);
-                res.ok(newInstance.toJSON());
+                if(!error) {
+
+                    if(results.userBalance >= results.totalPrize) {
+
+                        async.parallel({
+                            withdrawal: function(done) {
+                                Withdrawal.create({
+                                    amount: results.totalPrize,
+                                    user: data.user,
+                                    withdrawalType: 'bingoCards'
+                                }).exec(function (error, results) {
+                                        if(!error) {
+                                            done(null, results);
+                                        } else {
+                                            done(error);
+                                        }
+                                    });
+                            },
+                            cards: function(done) {
+                                // Create new instance of model using data from params
+                                BingoCard.create(data).exec(function created (err, newInstance) {
+
+                                    // Differentiate between waterline-originated validation errors
+                                    // and serious underlying issues. Respond with badRequest if a
+                                    // validation error is encountered, w/ validation info.
+                                    if (err) return res.negotiate(err);
+
+                                    // If we have the pubsub hook, use the model class's publish method
+                                    // to notify all subscribers about the created item
+                                    if (req._sails.hooks.pubsub) {
+                                        if (req.isSocket) {
+                                            Model.subscribe(req, newInstance);
+                                            Model.introduce(newInstance);
+                                        }
+
+                                        Model.publishCreate(newInstance, !req.options.mirror && req);
+                                    }
+
+
+                                    done(null, newInstance);
+
+
+                                });
+                            }
+                        }, function(error, results) {
+                            console.log(error);
+                            console.log(results);
+
+                            if(!error) {
+                                res.status(201);
+                                res.ok(results.cards.toJSON());
+                            }
+                        });
+                    }  else {
+                        res.json(500, { error: "Not enough balance" });
+                    }
+                }
             });
         } else {
-            res.forbidden('Need to be logged in to buy cards');
+            res.json(500, { error: "Need to be logged in to buy cards" });
         }
     }
 };
