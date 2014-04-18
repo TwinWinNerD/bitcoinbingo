596c210b50508618bd06c78ea0363d99ff73f387
Martijn Buurman <martijn@m19.nl>
2014-04-18 10:17:37 +0200

    Added UserService with getBalance method

---
+++ api/services/UserService.js
@@ -0,0 +1,39 @@
+var Q, async;
+
+Q = require('q');
+async= require('async');
+
+exports.getBalance = function (userId) {
+    var deferred;
+
+    deferred = Q.defer();
+
+    async.parallel({
+        deposits: function (done) {
+            Deposit.find().where( {user: userId }).sum('amount').exec(function (error, sum) {
+                if(!error) {
+                    done(null, sum[0].amount);
+                } else {
+                    done(error);
+                }
+            });
+        },
+        withdrawals: function (done) {
+            Withdrawal.find().where( {user: userId }).sum('amount').exec(function (error, sum) {
+                if(!error) {
+                    done(null, sum[0].amount);
+                } else {
+                    done(error);
+                }
+            });
+        }
+    }, function (error, results) {
+        if (!error) {
+            deferred.resolve(results.deposits - results.withdrawals);
+        } else {
+            deferred.reject("Error while determining balance");
+        }
+    });
+
+    return deferred.promise;
+};
