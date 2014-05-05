b42313b1b0e00e4d784bb674b9561bf361920f86
Martijn Buurman <martijn@m19.nl>
2014-05-05 17:30:14 +0200

    Revert 934840a..a330e57
    This rolls back to commit 934840ad24af14a04d2beab5c224c9b35e8f1230.

--- config/adapters.js
+++
@@ -1,8 +0,0 @@
-module.exports.adapters = {
-    'default': 'mongo',
-
-    mongo: {
-        module   : 'sails-mongo',
-        url      : 'mongodb://heroku:fG2ISUVuL9p84M3jfn0JgccmE8nu-1FYnJ9xG77WoEfxht33MNyG_Drl6bfSANFusVOk_nM-jat1sWSwL7vakw@lennon.mongohq.com:10028/app24881659'
-    }
-};
