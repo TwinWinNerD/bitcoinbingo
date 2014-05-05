967e1b5e4f5b43c016ec0964aae47956c406403f
Martijn Buurman <martijn@m19.nl>
2014-05-05 11:15:26 +0200

    Create a new message
    Storing the username as a string instead of association because we
    might want to store other messages too (like system messages)

--- api/controllers/MessageController.js
+++ api/controllers/MessageController.js
@@ -5,6 +5,44 @@
  * @docs        :: http://sailsjs.org/#!documentation/controllers
  */
 
+var actionUtil;
+
+actionUtil = require('../actionUtil');
+
 module.exports = {
-	
+    create: function(req, res) {
+        var Model = actionUtil.parseModel(req);
+
+        // Create data object (monolithic combination of all parameters)
+        // Omit the blacklisted params (like JSONP callback param, etc.)
+        var data = actionUtil.parseValues(req);
+
+        if(typeof req.user !== "undefined") {
+            data.user = req.user.username;
+
+            Message.create(data).exec(function created (err, newInstance) {
+
+                // Differentiate between waterline-originated validation errors
+                // and serious underlying issues. Respond with badRequest if a
+                // validation error is encountered, w/ validation info.
+                if (err) return res.negotiate(err);
+
+                // If we have the pubsub hook, use the model class's publish method
+                // to notify all subscribers about the created item
+                if (req._sails.hooks.pubsub) {
+                    if (req.isSocket) {
+                        Model.subscribe(req, newInstance);
+                        Model.introduce(newInstance);
+                    }
+
+                    Model.publishCreate(newInstance, !req.options.mirror && req);
+                }
+
+                res.ok(newInstance.toJSON());
+
+
+            });
+
+        }
+    }
 };
