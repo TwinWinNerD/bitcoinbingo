ff1dcaff3a460e1a75b2f12b4c8b60e804fbf392
Martijn Buurman <martijn@m19.nl>
2014-05-05 19:29:14 +0200

    Added register form

--- assets/js/scripts/routes/ApplicationRoute.js
+++ assets/js/scripts/routes/ApplicationRoute.js
@@ -1,1 +1,13 @@
-App.ApplicationRoute = Ember.Route.extend(Ember.SimpleAuth.ApplicationRouteMixin);
+App.ApplicationRoute = Ember.Route.extend(Ember.SimpleAuth.ApplicationRouteMixin, {
+    beforeModel: function () {
+        console.log(this.get('session.isAuthenticated'));
+        if(!this.get('session.isAuthenticated')) {
+            this.transitionTo('register');
+        }
+    },
+    actions: {
+        register: function () {
+            console.log("test");
+        }
+    }
+});
