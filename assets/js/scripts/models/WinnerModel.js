cd5efb21c7a0c659b86dc3d7ce6b785914246b11
Martijn Buurman <martijn@m19.nl>
2014-05-07 12:03:33 +0200

    Updated Ember Winner model

--- assets/js/scripts/models/WinnerModel.js
+++ assets/js/scripts/models/WinnerModel.js
@@ -1,4 +1,7 @@
 App.Winner = DS.Model.extend({
+	winnerType: DS.attr(),
+	amount: DS.attr(),
+	bingoCard: DS.belongsTo('bingoCard'),
     user: DS.belongsTo('user'),
     game: DS.belongsTo('game')
 });
