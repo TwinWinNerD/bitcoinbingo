7f9856dd5ce22e5441150ab564dee9ca7959d9f1
Martijn Buurman <martijn@m19.nl>
2014-05-17 14:42:49 +0200

    Revert "Only load idle and playing games on the homepage"
    This reverts commit 8f12eaea9ed27d97ab3b1d979b4311e11bd8b886.

--- assets/js/scripts/routes/GamesRoute.js
+++ assets/js/scripts/routes/GamesRoute.js
@@ -1,13 +1,5 @@
 App.GamesRoute = Ember.Route.extend({
     model: function () {
-        return Ember.RSVP.hash({
-            idleGames: this.store.find('game', { gameStatus: "idle" }),
-            playingGames: this.store.find('game', { gameStatus: "playing" })
-        });
-    },
-    setupController: function(controller, model) {
-        controller.set('idleGames', model.idleGames);
-        controller.set('playingGames', model.playingGames);
+        return this.store.find('game');
     }
-
 });
