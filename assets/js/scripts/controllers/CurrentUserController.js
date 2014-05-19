253261c2279160df0486a1b6fbc4d7340395cbc8
m19 <martijn@m19.nl>
2014-05-19 15:41:01 +0200

    Added balance attribute. Closes #3

--- assets/js/scripts/controllers/CurrentUserController.js
+++ assets/js/scripts/controllers/CurrentUserController.js
@@ -1,5 +1,10 @@
 App.CurrentUserController = Ember.ArrayController.extend({
+    needs: ['wallet'],
     username: function () {
         return this.get('session.username');
-    }.property('username')
+    }.property('username'),
+
+    balance: function () {
+        return satoshiToMBTC(this.get('controllers.wallet.balance'));
+    }.property('balance', 'controllers.wallet.balance')
 });
--- assets/js/scripts/controllers/WalletController.js
+++ assets/js/scripts/controllers/WalletController.js
@@ -7,8 +7,29 @@ App.WalletController = Ember.ArrayController.extend({
     }.property('deposits'),
     userWithdrawals: function () {
         var userId = this.get('session.content.id');
-        return this.store.filter('deposit', function(deposit) {
-            return deposit.get('user.id') === userId;
+        return this.store.filter('withdrawal', function(withdrawal) {
+            return withdrawal.get('user.id') === userId;
         });
-    }.property('withdrawals')
+    }.property('withdrawals'),
+    balance: function () {
+        var deposits, totalDeposits, withdrawals, totalWithdrawals;
+
+        deposits = this.get('userDeposits');
+        withdrawals = this.get('userWithdrawals');
+
+
+        totalDeposits = deposits.reduce(function (prev, deposit) {
+            return prev + deposit.get('amount');
+        }, 0);
+
+        totalWithdrawals = withdrawals.reduce(function (prev, withdrawal) {
+            return prev + withdrawal.get('amount');
+        }, 0);
+
+        console.log(totalDeposits);
+        console.log(totalWithdrawals);
+
+        return totalDeposits - totalWithdrawals;
+
+    }.property('userDeposits.@each.amount', 'userWithdrawals.@each.amount')
 });
