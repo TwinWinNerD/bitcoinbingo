54837e4e0223d45cce956bafa36a2e3310784e1e
m19 <martijn@m19.nl>
2014-05-19 13:49:31 +0200

    Added deposits and withdrawals to user

--- assets/js/scripts/models/UserModel.js
+++ assets/js/scripts/models/UserModel.js
@@ -2,5 +2,7 @@ App.User = DS.Model.extend({
     username: DS.attr(),
     clientSeed: DS.attr(),
     bingoCards: DS.hasMany('bingoCard'),
+    deposits: DS.hasMany('deposit'),
+    withdrawals: DS.hasMany('withdrawal'),
     winners: DS.hasMany('winner')
 });
