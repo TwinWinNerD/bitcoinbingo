4d6fe7d5d09c847f4a84a4ce33923e718700de49
m19 <martijn@m19.nl>
2014-06-02 17:36:44 +0200

    Removed async

--- assets/js/scripts/models/GameModel.js
+++ assets/js/scripts/models/GameModel.js
@@ -5,8 +5,8 @@ App.Game = DS.Model.extend({
     pattern: DS.attr(),
     gameStatus: DS.attr(),
     table: DS.belongsTo('table'),
-    bingoCards: DS.hasMany('bingoCard', { async: true }),
-    messages: DS.hasMany('message', { async: true }),
+    bingoCards: DS.hasMany('bingoCard'),
+    messages: DS.hasMany('message'),
     winners: DS.hasMany('winner'),
     createdAt: DS.attr(),
     updatedAt: DS.attr()
