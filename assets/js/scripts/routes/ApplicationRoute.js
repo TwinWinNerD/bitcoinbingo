3cda8bc0caa9ce80d202ad19f0d835c05b1bc4ad
m19 <martijn@m19.nl>
2014-06-03 16:55:20 +0200

    Always load user balance. Closes #28

--- assets/js/scripts/routes/ApplicationRoute.js
+++ assets/js/scripts/routes/ApplicationRoute.js
@@ -3,5 +3,19 @@ App.ApplicationRoute = Ember.Route.extend(Ember.SimpleAuth.ApplicationRouteMixin
         if(!this.get('session.isAuthenticated')) {
             this.transitionTo('register');
         }
+    },
+    model: function () {
+        if(this.get('session.isAuthenticated')) {
+            return Ember.RSVP.hash({
+                deposits: this.store.find('deposit', { user: this.get('session.id') }),
+                withdrawals: this.store.find('withdrawal', { user: this.get('session.id') })
+            });
+        }
+    },
+    setupController: function (controller, model) {
+        if(this.get('session.isAuthenticated')) {
+            this.controllerFor('wallet').set('deposits', model.deposits);
+            this.controllerFor('wallet').set('withdrawals', model.withdrawals);
+        }
     }
 });
--- assets/js/scripts/routes/LoginRoute.js
+++ assets/js/scripts/routes/LoginRoute.js
@@ -7,6 +7,13 @@ App.LoginRoute = Ember.Route.extend({
         // display an error when authentication fails
         sessionAuthenticationFailed: function(message) {
             this.controller.set('errorMessage', message);
+        },
+        sessionAuthenticationSucceeded: function() {
+            Ember.RSVP.hash({
+                deposits: this.store.find('deposit', { user: this.get('session.id') }),
+                withdrawals: this.store.find('withdrawal', { user: this.get('session.id') })
+            });
+            this.transitionTo('games');
         }
     }
 });
