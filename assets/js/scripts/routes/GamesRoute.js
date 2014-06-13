6372e12fef52c7c58cddcbb6924cadfc13d085b6
m19 <martijn@m19.nl>
2014-06-13 13:41:32 +0200

    Since we receive updates via sockets prevent ember from calling the server after switching routes

--- assets/js/scripts/routes/GamesRoute.js
+++ assets/js/scripts/routes/GamesRoute.js
@@ -1,22 +1,29 @@
 App.GamesRoute = Ember.Route.extend({
     model: function () {
-        return Ember.RSVP.hash({
-            modelIdleGames: this.store.find('game', { gameStatus: 'idle' }),
-            modelCountDownGames: this.store.find('game', { gameStatus: 'countDown' }),
-            modelPlayingGames: this.store.find('game', { gameStatus: 'playing' })
-        });
+        if(!this.controllerFor('games').get('loadedGames')) {
+            return Ember.RSVP.hash({
+                modelIdleGames: this.store.find('game', { gameStatus: 'idle' }),
+                modelCountDownGames: this.store.find('game', { gameStatus: 'countDown' }),
+                modelPlayingGames: this.store.find('game', { gameStatus: 'playing' })
+            });
+        } else {
+            return Ember.RSVP.hash({
+                modelIdleGames: this.store.filter('game', function (game) {
+                    return (game.get('gameStatus') === "idle")
+                }),
+                modelCountDownGames: this.store.filter('game', function (game) {
+                    return (game.get('gameStatus') === "countDown")
+                }),
+                modelPlayingGames: this.store.filter('game', function (game) {
+                    return (game.get('gameStatus') === "playing");
+                })
+            });
+        }
     },
     setupController: function (controller, model) {
         controller.set('modelIdleGames', model.modelIdleGames);
         controller.set('modelCountDownGames', model.modelCountDownGames);
         controller.set('modelPlayingGames', model.modelPlayingGames);
-    },
-    afterModel: function () {
-        if(this.get('session.isAuthenticated')) {
-            return Ember.RSVP.hash({
-                deposits: this.store.find('deposit', { user: this.get('session.id') }),
-                withdrawals: this.store.find('withdrawal', { user: this.get('session.id') })
-            });
-        }
+        controller.set('loadedGames', true);
     }
 });
--- assets/js/scripts/routes/WalletRoute.js
+++ assets/js/scripts/routes/WalletRoute.js
@@ -1,13 +1,27 @@
 App.WalletRoute = Ember.Route.extend({
     model: function () {
-        return Ember.RSVP.hash({
-            deposits: this.store.find('deposit', { user: this.get('session.id') }),
-            withdrawals: this.store.find('withdrawal', { user: this.get('session.id') })
-        });
+        if(!this.controllerFor('wallet').get('loadedWallet')) {
+            return Ember.RSVP.hash({
+                deposits: this.store.find('deposit', { user: this.get('session.id') }),
+                withdrawals: this.store.find('withdrawal', { user: this.get('session.id') })
+            });
+        } else {
+            var userId = this.get('session.content.id');
+
+            return Ember.RSVP.hash({
+                deposits: this.store.filter('deposit', function(withdrawal) {
+                    return withdrawal.get('user.id') === userId;
+                }),
+                withdrawals: this.store.filter('withdrawal', function(withdrawal) {
+                    return withdrawal.get('user.id') === userId;
+                })
+            });
+        }
     },
 
     setupController: function (controller, model) {
         controller.set('deposits', model.deposits);
         controller.set('withdrawals', model.withdrawals);
+        controller.set('loadedWallet', true);
     }
 });
