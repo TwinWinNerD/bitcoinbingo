b92b5a704da012a71c9a771d9858541b3da5e562
m19 <martijn@m19.nl>
2014-06-19 15:56:58 +0200

    Added protectedAttributes policy

---
+++ api/policies/protectedAttributes.js
@@ -0,0 +1,32 @@
+/**
+ * protectedAttributes
+ *
+ * @module      :: Policy
+ * @description :: Simple policy to protect certain attributes as returned by Model.protectedAttributes()
+ * @docs        :: http://sailsjs.org/#!documentation/policies
+ *
+ */
+
+var actionUtil = require( './../actionUtil' );
+
+module.exports = function ( req, res, next ) {
+
+    var Model = actionUtil.parseModel( req );
+
+    if ( Model.protectedAttributes ) {
+        var attributes = Model.protectedAttributes();
+        _.each( attributes, function ( attr ) {
+            if ( req.params.hasOwnProperty( attr ) ) {
+                delete req.params[ attr ];
+            }
+            if ( req.query.hasOwnProperty( attr ) ) {
+                delete req.query[ attr ];
+            }
+            if ( req.body.hasOwnProperty( attr ) ) {
+                delete req.body[ attr ];
+            }
+
+        } );
+    }
+    return next();
+};
