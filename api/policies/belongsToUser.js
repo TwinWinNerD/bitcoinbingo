28fe9b182c254f5dd5109450a54542328ec9a527
m19 <martijn@m19.nl>
2014-06-19 16:22:49 +0200

    Added belongsToUser policy for #27

---
+++ api/policies/belongsToUser.js
@@ -0,0 +1,10 @@
+module.exports = function(req, res, next) {
+
+    var user = req.options.user || (req.options.where && req.options.where.user) || req.param('user');
+
+    if(user === req.session.user.id) {
+        return next();
+    }
+
+    return res.forbidden();
+};
