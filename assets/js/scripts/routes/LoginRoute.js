680c492d5af2d46efec31be73214a252926d76ce
m19 <martijn@m19.nl>
2014-06-19 17:46:50 +0200

    Removed code that caused a bug. Also not needed

--- assets/js/scripts/routes/LoginRoute.js
+++ assets/js/scripts/routes/LoginRoute.js
@@ -1,13 +1,5 @@
 App.LoginRoute = Ember.Route.extend({
     setupController: function(controller, model) {
         controller.set('errorMessage', null);
-    },
-    actions: {
-        sessionAuthenticationSucceeded: function() {
-            var self = this;
-            return this.store.find('user', { user: this.get('session.id') }).then(function () {
-                self.transitionTo('games');
-            });
-        }
     }
 });
