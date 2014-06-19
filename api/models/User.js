efd2f88056d68eeea8c13a7cf7e7ed698f1a10a1
m19 <martijn@m19.nl>
2014-06-19 14:29:14 +0200

    Never send password to the front-end.

--- api/models/User.js
+++ api/models/User.js
@@ -65,6 +65,12 @@ module.exports = {
         games: {
             collection: 'game',
             via: 'users'
+        },
+
+        toJSON: function() {
+            var obj = this.toObject();
+            delete obj.password;
+            return obj;
         }
     },
 
