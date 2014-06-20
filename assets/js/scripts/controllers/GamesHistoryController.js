ccfecbaf6dbafad15b3eb424739630ab90824de5
m19 <martijn@m19.nl>
2014-06-20 20:31:26 +0200

    Revert "Reverse game history, closes #47"
    This reverts commit 5f6d74bc35080eafd47f0a32e4abb92d01107099.

--- assets/js/scripts/controllers/GamesHistoryController.js
+++ assets/js/scripts/controllers/GamesHistoryController.js
@@ -1,10 +1,3 @@
 App.GamesHistoryController = Ember.ArrayController.extend({
-    itemController: 'game',
-    lastPersonalGamesSorted: function () {
-        return Ember.ArrayProxy.createWithMixins(Ember.SortableMixin, {
-            sortProperties: ['updatedAt'],
-            sortAscending: false,
-            content: this.get('lastPersonalGames')
-        });
-    }.property('lastPersonalGames.[]')
+    itemController: 'game'
 });
