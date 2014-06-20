6265f29e5cbfbf12a9467f80f3948db4c6b0434c
m19 <martijn@m19.nl>
2014-06-20 20:30:41 +0200

    Limit games history for now to last 20

--- assets/js/scripts/routes/GamesHistoryRoute.js
+++ assets/js/scripts/routes/GamesHistoryRoute.js
@@ -1,7 +1,7 @@
 App.GamesHistoryRoute = Ember.Route.extend(Ember.SimpleAuth.AuthenticatedRouteMixin, {
     model: function () {
         return Ember.RSVP.hash({
-            lastPersonalGames: this.store.find('game', { user: this.get('session.id') })
+            lastPersonalGames: this.store.find('game', { user: this.get('session.id'), limit: 20, sort: 'updatedAt DESC' })
         });
     },
     setupController: function (controller, model) {
