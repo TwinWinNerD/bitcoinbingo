4aec2e64ff012fb0ee8b387e6e975bd159222568
m19 <martijn@m19.nl>
2014-07-07 11:55:58 +0200

    Added updateBalance method

--- api/services/UserService.js
+++ api/services/UserService.js
@@ -3,14 +3,14 @@ var Q, async;
 Q = require('q');
 async= require('async');
 
-exports.getBalance = function (userId) {
-    var deferred;
+exports.getBalance = function (userId, confirmed) {
+    var deferred = Q.defer();
 
-    deferred = Q.defer();
+    if(!confirmed) confirmed = 0;
 
     async.parallel({
         deposits: function (done) {
-            Deposit.find().where( {user: userId }).sum('amount').exec(function (error, sum) {
+            Deposit.find().where( {user: userId, confirmed: { '>=': confirmed } }).sum('amount').exec(function (error, sum) {
                 if(!error) {
                     if(sum.length > 0) {
                         done(null, sum[0].amount);
@@ -41,6 +41,37 @@ exports.getBalance = function (userId) {
         } else {
             deferred.reject("Error while determining balance");
         }
+    });
+
+    return deferred.promise;
+};
+
+exports.updateBalance = function (userId, amount) {
+    var deferred = Q.defer();
+
+    exports.getBalance(userId, 0).then(function (balance) {
+        balance = Number(balance);
+        amount = Number(amount);
+
+        console.log(balance);
+        console.log(amount);
+
+        if(amount < 0) {
+            balance -= amount;
+        } else {
+            balance += amount;
+        }
+        console.log(balance);
+
+        User.update(userId, { balance: balance }).exec(function (err, result) {
+            if(!err && result) {
+                User.publishUpdate(userId, { balance: balance }, null);
+
+                deferred.resolve(result);
+            } else {
+                deferred.reject(err);
+            }
+        });
     });
 
     return deferred.promise;
