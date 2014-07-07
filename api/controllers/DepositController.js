b7ef5ff4f9bdd86a08440902b9d7b7faa68095ae
m19 <martijn@m19.nl>
2014-07-07 12:59:51 +0200

    Added Deposit callback function

--- api/controllers/DepositController.js
+++ api/controllers/DepositController.js
@@ -6,5 +6,68 @@
  */
 
 module.exports = {
-	
+
+    callback: function (req, res) {
+        var amount = req.param('value');
+        var address = req.param('input_address');
+        var hash = req.param('transaction_hash');
+        var confirmations = req.param('confirmations');
+        var confirmed = (confirmations > 0) ? 1 : 0;
+        var secret = req.param('secret');
+
+        if(secret !== sails.config.blockchain.secret) return res.badRequest('invalid secret');
+
+        User.findOne().where({ depositAddress: address }).exec(function (err, user) {
+            if(!err && user) {
+
+                Deposit.findOne().where({ hash: hash}).exec(function (err, deposit) {
+
+                    if(!err && !deposit) {
+
+                        Deposit.create({
+                            amount: amount,
+                            depositType: 'bitcoin',
+                            hash: hash,
+                            confirmed: confirmed,
+                            user: user.id
+                        }).exec(function (err, deposit) {
+
+                            if(!err && deposit) {
+
+                                UserService.updateBalance(user.id, 0).then(function (result) {
+                                    Deposit.publishCreate(deposit.toJSON());
+
+                                    if(confirmed > 0) {
+                                        return res.ok('ok');
+                                    }
+
+                                    return res.badRequest();
+                                });
+
+                            } else {
+                                return res.badRequest();
+                            }
+                        });
+                    } else if(deposit) {
+
+                        if(confirmed > 0) {
+                            Deposit.update(deposit.id, { confirmed: confirmed }).exec(function (err, result) {
+                                if(!err && result) {
+
+                                    Deposit.publishUpdate(deposit.id, { confirmed: confirmed }, null);
+                                    return res.ok('ok');
+                                } else {
+                                    return res.badRequest();
+                                }
+                            });
+                        } else {
+                            return res.badRequest();
+                        }
+                    }
+                });
+            } else {
+                return res.badRequest();
+            }
+        });
+    }
 };
