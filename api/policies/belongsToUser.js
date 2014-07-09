ce27fc55187341ddef987a73d099fb306eb7e2e0
m19 <martijn@m19.nl>
2014-07-09 18:33:45 +0200

    Fix conflict with string integer comparison

--- api/policies/belongsToUser.js
+++ api/policies/belongsToUser.js
@@ -2,7 +2,7 @@ module.exports = function(req, res, next) {
 
     var user = req.options.user || (req.options.where && req.options.where.user) || req.param('user');
 
-    if(user === req.session.user.id) {
+    if(user == req.session.user.id) {
         return next();
     }
 
