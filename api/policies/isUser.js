c0b47ebda44eac966f05d212a0ce5f9a584132a9
m19 <martijn@m19.nl>
2014-07-09 10:52:23 +0200

    Fixed problem with string to int comparison

--- api/policies/isUser.js
+++ api/policies/isUser.js
@@ -2,7 +2,7 @@ module.exports = function(req, res, next) {
 
     var id = req.options.id || (req.options.where && req.options.where.id) || req.param('id');
 
-    if(id === req.session.user.id) {
+    if(id == req.session.user.id) {
         return next();
     }
 
