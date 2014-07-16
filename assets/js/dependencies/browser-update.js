e92d4da8ebf0f400e449c024f5d3e3e321194a74
m19 <martijn@m19.nl>
2014-07-16 18:58:35 +0200

    Added browser update script.

---
+++ assets/js/dependencies/browser-update.js
@@ -0,0 +1,9 @@
+var $buoop = {vs:{i:9,f:15,o:12.1,s:5.1}};
+$buoop.ol = window.onload;
+window.onload = function(){
+    try {if ($buoop.ol) $buoop.ol();}catch (e) {}
+    var e = document.createElement("script");
+    e.setAttribute("type", "text/javascript");
+    e.setAttribute("src", "//browser-update.org/update.js");
+    document.body.appendChild(e);
+};
