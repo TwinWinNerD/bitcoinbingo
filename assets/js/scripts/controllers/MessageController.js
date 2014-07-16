e1cf0bd72cebf1d09e26c20f0f647f4cc52f4211
m19 <martijn@m19.nl>
2014-07-16 20:45:25 +0200

    Add type to messages

--- assets/js/scripts/controllers/MessageController.js
+++ assets/js/scripts/controllers/MessageController.js
@@ -1,2 +1,8 @@
 App.MessageController = Ember.ObjectController.extend({
+    isUserMessage: function () {
+        return this.get('model.type') === 'user';
+    }.property('model.type'),
+    isSystemMessage: function () {
+        return this.get('model.type') === 'system';
+    }.property('model.type')
 });
