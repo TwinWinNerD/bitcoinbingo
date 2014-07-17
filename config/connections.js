0d7f22c4c6bc6a8f5ce6c58d93c12559f9c90283
m19 <martijn@m19.nl>
2014-07-17 15:15:45 +0200

    Fixed connection

--- config/connections.js
+++ config/connections.js
@@ -34,8 +34,8 @@ module.exports.connections = {
         host     : process.env.DB_HOST,
         port     : process.env.DB_PORT,
         user     : process.env.DB_USER,
-        password : process.env.DB_PASSWORD,
-        database : process.env.DB_DATABASE
+        password : process.env.DB_PASS,
+        database : process.env.DB_NAME
     }
 };
 
