6d34430aadbf7045efa255a309b280f85bb95227
m19 <martijn@m19.nl>
2014-07-21 18:42:42 +0200

    Added deposit address QR code

--- assets/js/scripts/views/WalletView.js
+++ assets/js/scripts/views/WalletView.js
@@ -1,7 +1,15 @@
 App.WalletView = Ember.View.extend({
     didInsertElement: function () {
-        $("#depositAddress").on('click', function () {
-            this.select();
+        var div = $("#depositAddress");
+
+        div.popover({
+            html: true,
+            placement: 'right',
+            content: function () {
+                var url = 'https://chart.googleapis.com/chart?chs=225x225&cht=qr&chl=' + div.text() + '&choe=UTF-8';
+                return '<img width="250" height="250" src=' + url + ' />';
+            },
+            trigger: 'hover'
         });
     }
 });
