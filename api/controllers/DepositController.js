edc26211563f82093b7f9e927648d59a9fd45bc9
m19 <martijn@m19.nl>
2014-07-22 12:27:30 +0200

    Revert "Revert "Revert "Added deposit bonus"""
    This reverts commit fb6ef368e303beaed8b075d5eb1975c809348aac.

--- api/controllers/DepositController.js
+++ api/controllers/DepositController.js
@@ -30,84 +30,50 @@ module.exports = {
 
         User.findOne().where({ depositAddress: address }).exec(function (err, user) {
             if(!err && user) {
-                async.series([
-                    function (done) {
-                        Deposit.findOne().where({ hash: hash}).exec(function (err, deposit) {
-                            if(!err && !deposit) {
-                                Deposit.create({
-                                    amount: amount,
-                                    depositType: 'Bitcoin',
-                                    hash: hash,
-                                    confirmed: confirmed,
-                                    user: user.id
-                                }).exec(function (err, deposit) {
 
-                                    if(!err && deposit) {
+                Deposit.findOne().where({ hash: hash}).exec(function (err, deposit) {
 
-                                        UserService.updateBalance(user.id, 0).then(function (result) {
-                                            Deposit.publishCreate(deposit.toJSON());
+                    if(!err && !deposit) {
 
-                                            if(confirmed > 0) {
-                                                done(null, true);
-                                            }
+                        Deposit.create({
+                            amount: amount,
+                            depositType: 'bitcoin',
+                            hash: hash,
+                            confirmed: confirmed,
+                            user: user.id
+                        }).exec(function (err, deposit) {
 
-                                            done(true);
-                                        });
+                            if(!err && deposit) {
 
-                                    } else {
-                                        done(true);
+                                UserService.updateBalance(user.id, 0).then(function (result) {
+                                    Deposit.publishCreate(deposit.toJSON());
+
+                                    if(confirmed > 0) {
+                                        return res.ok('*ok*');
                                     }
+
+                                    return res.badRequest();
                                 });
-                            } else if(deposit) {
 
-                                if(confirmed > 0) {
-                                    Deposit.update(deposit.id, { confirmed: confirmed }).exec(function (err, result) {
-                                        if(!err && result) {
-
-                                            Deposit.publishUpdate(deposit.id, { confirmed: confirmed }, null);
-                                            done(null, true);
-                                        } else {
-                                            done(true);
-                                        }
-                                    });
-                                } else {
-                                    done(true);
-                                }
-                            }
-                        });
-                    },
-                    function (done) {
-                        Deposit.find().where({ user: user.id }).exec(function (err, deposits) {
-                            if(!err && deposits.length <= 1 && amount < 1000000) {
-                                Deposit.create({
-                                    amount: amount,
-                                    depositType: 'Promotion',
-                                    confirmed: 1,
-                                    user: user.id
-                                }).exec(function (err, deposit) {
-                                    if(!err && deposit) {
-
-                                        UserService.updateBalance(user.id, 0).then(function (result) {
-                                            Deposit.publishCreate(deposit.toJSON());
-
-                                            done(null, true);
-
-                                        });
-
-                                    } else {
-                                        done(true);
-                                    }
-                                });
                             } else {
-                                done(true);
+                                return res.badRequest();
                             }
                         });
-                    }
-                ], function (err, result) {
-                    if(!err && result) {
-                        return res.ok('*ok*');
-                    } else {
-                        return res.badRequest();
+                    } else if(deposit) {
+
+                        if(confirmed > 0) {
+                            Deposit.update(deposit.id, { confirmed: confirmed }).exec(function (err, result) {
+                                if(!err && result) {
+
+                                    Deposit.publishUpdate(deposit.id, { confirmed: confirmed }, null);
+                                    return res.ok('*ok*');
+                                } else {
+                                    return res.badRequest();
+                                }
+                            });
+                        } else {
+                            return res.badRequest();
+                        }
                     }
                 });
             } else {
