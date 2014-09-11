13881915268afb5bb32c5fe411fc7fc3aa554295
m19 <martijn@m19.nl>
2014-09-11 21:18:34 +0200

    Added migrate option

--- config/models.js
+++ config/models.js
@@ -8,11 +8,8 @@
 util = require('util');
 
 module.exports.models = {
- 
-  // Your app's default connection.
-  // i.e. the name of one of your app's connections (see `config/connections.js`)
-  //
-  // (defaults to localDiskDb)
+  migrate: 'alter',
+
   connection: 'bitcoinBingoMySQL',
 
     publishCreate: function(values, req, options) {
