57352da34fbfc04cabbabd0b4847236087d9ca3c
m19 <martijn@m19.nl>
2014-09-13 19:23:38 +0200

    Cleaned up BingoCard model

--- api/models/BingoCard.js
+++ api/models/BingoCard.js
@@ -1,40 +1,24 @@
-/**
- * BingoCard.js
- *
- * @description :: TODO: You might write a short summary of how this model works and what it represents here.
- * @docs    :: http://sailsjs.org/#!documentation/models
- */
-
 module.exports = {
-
   attributes: {
-
     clientSeed: {
       type: 'STRING',
       maxLength: 20,
       notNull: true
     },
-
     game: {
       model: 'game',
       required: true
     },
-
     user: {
       model: 'user',
       required: true
     },
-
     toJSON: function () {
-      var obj;
-
-      obj = this.toObject();
+      var obj = this.toObject();
 
       obj.squares = BingoCardService.generateSquares(obj.game.serverSeed, obj.clientSeed, obj.id);
 
       return obj;
     }
-
   }
-
 };
