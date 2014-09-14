653b1cd05bd780dfa098587b16fc0ac830622697
m19 <martijn@m19.nl>
2014-09-14 19:46:26 +0200

    Cleaned up user model

--- api/models/User.js
+++ api/models/User.js
@@ -1,93 +1,68 @@
-/**
- * User.js
- *
- * @description :: TODO: You might write a short summary of how this model works and what it represents here.
- * @docs        :: http://sailsjs.org/#!documentation/models
- */
-
-var bcrypt;
-
-bcrypt = require('bcrypt');
+var bcrypt = require('bcrypt');
 
 module.exports = {
-
   attributes: {
-
     username: {
       type: 'STRING',
       unique: true,
       minLength: 3,
       required: true
     },
-
     email: {
       type: 'STRING',
       email: true,
       unique: true,
       required: false
     },
-
     password: {
       type: 'STRING'
     },
-
     clientSeed: {
       type: 'STRING',
       required: false,
       maxLength: 20,
       defaultsTo: ""
     },
-
     depositAddress: {
       type: 'STRING',
       defaultsTo: ""
     },
-
     balance: {
       type: 'INTEGER',
       defaultsTo: 0
     },
-
     registrationIp: {
       type: 'STRING'
     },
-
     bingoCards: {
       collection: 'bingoCard',
       via: 'user'
     },
-
     deposits: {
       collection: 'deposit',
       via: 'user'
     },
-
     withdrawals: {
       collection: 'withdrawal',
       via: 'user'
     },
-
     winners: {
       collection: 'winner',
       via: 'user'
     },
-
     games: {
       collection: 'game',
       via: 'users'
     },
-
     toJSON: function () {
       var obj = this.toObject();
       delete obj.password;
       return obj;
     }
   },
-
   protectedAttributes: function () {
     return [ "username", "balance", "bingoCards", "games", "winners", "deposits", "withdrawals", "depositAddress" ];
   },
-
   beforeCreate: function (user, next) {
     if (user.hasOwnProperty('password')) {
       bcrypt.hash(user.password, 10, function (err, hash) {
@@ -98,7 +73,6 @@ module.exports = {
       next(null, user);
     }
   },
-
   beforeUpdate: function (user, next) {
     if (user.hasOwnProperty('password')) {
       bcrypt.hash(user.password, 10, function (err, hash) {
@@ -109,5 +83,4 @@ module.exports = {
       next(null, user);
     }
   }
-
 };
