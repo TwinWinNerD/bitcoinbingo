cdeb795ea8cbc1e2887bd9e4b117d2e578f8ba83
m19 <martijn@m19.nl>
2014-09-15 17:29:05 +0200

    Added jackpot and houseEdge attributes

--- api/models/Table.js
+++ api/models/Table.js
@@ -1,49 +1,42 @@
-/**
- * Table.js
- *
- * @description :: TODO: You might write a short summary of how this model works and what it represents here.
- * @docs    :: http://sailsjs.org/#!documentation/models
- */
-
 module.exports = {
-
   attributes: {
-
     minimumPlayers: {
-      type: 'INTEGER',
+      type: 'integer',
       integer: true,
       required: true
     },
-
     maximumPlayers: {
-      type: 'INTEGER',
+      type: 'integer',
       integer: true,
       required: true
     },
-
     maximumCards: {
-      type: 'INTEGER',
+      type: 'integer',
       integer: true,
       required: true
     },
-
     cardPrice: {
-      type: 'INTEGER',
+      type: 'integer',
       integer: true,
       required: true
     },
-
+    jackpot: {
+      type: 'integer',
+      integer: true,
+      defaultsTo: 0
+    },
+    houseEdge: {
+      type: 'integer',
+      defaultsTo: 1
+    },
     tableType: {
-      type: 'STRING',
+      type: 'string',
       text: true,
       required: true
     },
-
     games: {
       collection: 'game',
       via: 'table'
     }
-
   }
-
 };
