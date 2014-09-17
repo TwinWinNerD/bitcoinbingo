3800ed70e3dfc1bc5fda9411df9b8849ecaca6af
m19 <martijn@m19.nl>
2014-09-17 19:34:14 +0200

    Added extra config

--- config/bingo.js
+++ config/bingo.js
@@ -1,3 +1,7 @@
 module.exports.bingo = {
   tableJackpotPercentage: 0.5,
+  ballsForTableJackpot: 48,
+  progressiveJackpotPercentage: 0.5,
+  ballsForProgressiveJackpot: 42,
+  houseEdgePercentage: 1
 };
