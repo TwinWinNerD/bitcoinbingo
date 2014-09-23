a1969b30ab2d069af53bddfb0e7b6d4b79576d88
m19 <martijn@m19.nl>
2014-09-23 13:23:16 +0200

    Added removePrizePool method

--- api/services/GameService.js
+++ api/services/GameService.js
@@ -44,3 +44,27 @@ exports.addPrizePool = function (gameId, amount) {
 
   return deferred.promise;
 };
+
+exports.removePrizePool = function (gameId, amount) {
+  var deferred = Q.defer();
+
+  var query = 'UPDATE `game` SET `game`.`prizePool`=`game`.`prizePool` - ' +
+    amount + ' WHERE id=' + gameId;
+
+  Game.query(query, function (err, result) {
+    if(!err && result) {
+      Game.findOne(gameId)
+        .exec(function (err, result) {
+          if(!err && result) {
+            deferred.resolve(result);
+          } else {
+            deferred.reject(err);
+          }
+        });
+    } else {
+      deferred.reject(err);
+    }
+  });
+
+  return deferred.promise;
+};
