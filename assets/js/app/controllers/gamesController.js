96bd8216731e73c5577c4e3981b3c5723184fcea
Martijn Buurman <martijn@m19.nl>
2014-10-11 13:05:30 +0200

    Removed unused code

--- assets/js/app/controllers/gamesController.js
+++ assets/js/app/controllers/gamesController.js
@@ -1,13 +1,7 @@
 function GamesController ($scope, $sailsSocket, $location) {
-  var app = this;
-
-  app.test = "test";
-  console.log("test controller");
-
   $scope.games = [];
 
   $sailsSocket.get('/api/game').then(function (result) {
-    console.log(result);
     $scope.games = result.data;
   });
 
