4c3747ee22cfe0081ff26cbf0b43bc42b0903d98
Martijn Buurman <martijn@m19.nl>
2014-10-13 08:15:14 +0200

    Added general Application controller

---
+++ assets/js/app/controllers/applicationController.js
@@ -0,0 +1,44 @@
+app
+  .controller('ApplicationController', function ($scope, $cookieStore, $location, $sailsSocket, Session, Auth) {
+    $scope.currentUser = $cookieStore.get('user') ? { id: $cookieStore.get('user') } : null;
+
+
+    $scope.setCurrentUser = function (user) {
+      $cookieStore.put('user', user.id);
+      $scope.currentUser = user;
+    };
+
+    $scope.deleteCurrentUser = function () {
+      $cookieStore.remove('user');
+      delete $scope.currentUser;
+    };
+
+    $scope.findCurrentUser = function () {
+      if ($cookieStore.get('user')) {
+        $sailsSocket.get('/api/user/' + $cookieStore.get('user'))
+          .success(function (result) {
+            if(result.isYou) {
+              $scope.currentUser = result;
+            } else {
+              $scope.deleteCurrentUser();
+            }
+          }).error(function () {
+            $scope.deleteCurrentUser();
+            $location.path('/register');
+          });
+      } else {
+        $location.path('/register');
+      }
+    };
+
+    $scope.logout = function () {
+      Auth.logout(function () {
+        $scope.deleteCurrentUser();
+        $location.path('/login');
+      }, function () {
+        $scope.error = "Couldn't logout";
+      });
+    };
+
+    $scope.findCurrentUser();
+  });
