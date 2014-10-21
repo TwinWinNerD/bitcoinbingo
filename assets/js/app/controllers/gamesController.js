04ee5f48ba9c8d2620bc531528c1ae93ac70be8d
Martijn Buurman <martijn@m19.nl>
2014-10-21 15:46:30 +0200

    Listen to updates

--- assets/js/app/controllers/gamesController.js
+++ assets/js/app/controllers/gamesController.js
@@ -13,6 +13,33 @@ function GamesController ($scope, $sailsSocket, $location) {
     }
   });
 
+  $sailsSocket.subscribe('game', function (result) {
+    if(result.verb === 'updated') {
+      for(var i = 0; i < $scope.idleGames.length; i++) {
+        var game = $scope.idleGames[i];
+
+        if(game.id === result.id) {
+          delete result.data.messages;
+          delete result.data.table;
+          delete result.data.bingoCards;
+          _.merge(game, result.data);
+        }
+      }
+    }
+  });
+
+  $sailsSocket.subscribe('table', function (result) {
+    if(result.verb === 'updated') {
+      for(var i = 0; i < $scope.idleGames.length; i++) {
+        var game = $scope.idleGames[i];
+
+        if(game.table.id === result.id) {
+          _.merge(game.table, result.data);
+        }
+      }
+    }
+  });
+
   $scope.go = function (game) {
     $location.path('/game/' + game.id);
   };
