a00eeb48a0571bf79454a0d61fd54006ace70a9b
Martijn Buurman <martijn@m19.nl>
2014-10-21 14:35:50 +0200

    Added calculatePrizePool and addUser methods

--- api/services/GameService.js
+++ api/services/GameService.js
@@ -1,5 +1,9 @@
 var Q = require('q');
 
+exports.calculatePrizePool = function (amount, houseEdge) {
+  return amount - ((amount / 100) * houseEdge);
+};
+
 exports.createGame = function (tableId) {
   var deferred = Q.defer();
 
@@ -32,6 +36,7 @@ exports.addPrizePool = function (gameId, amount) {
       Game.findOne(gameId)
         .exec(function (err, result) {
           if(!err && result) {
+            Game.publishUpdate(gameId, result);
             deferred.resolve(result);
           } else {
             deferred.reject(err);
@@ -61,6 +66,41 @@ exports.removePrizePool = function (gameId, amount) {
             deferred.reject(err);
           }
         });
+    } else {
+      deferred.reject(err);
+    }
+  });
+
+  return deferred.promise;
+};
+
+exports.addUser = function (gameId, userId) {
+  var deferred = Q.defer();
+
+  Game.findOne(gameId)
+    .populate('users')
+    .exec(function (err, result) {
+    if (!err && result) {
+      var foundUser = false;
+      for (var i = 0; i < result.users.length; i++) {
+        if (result.users[i].id === userId) {
+          foundUser = true;
+        }
+      }
+
+      if (foundUser) {
+        deferred.resolve();
+      } else {
+        result.users.add(userId);
+        result.save(function (err, result) {
+          if (!err && result) {
+            Game.publishUpdate(gameId, result);
+            deferred.resolve();
+          } else {
+            deferred.reject(err);
+          }
+        });
+      }
     } else {
       deferred.reject(err);
     }
